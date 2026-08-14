9825d2487b3e8ee12e6bf94706290cd1ad0854e4
Perry Jacobs <sallydon164@gmail.com>
2026-08-14 19:05:24 +0000

    added static files to server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import type { Request, Response, Express } from 'express';
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.get('/', (req: Request, res: Response): void => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
